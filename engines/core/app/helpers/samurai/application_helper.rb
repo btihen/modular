14ee7233ddce6c3601c65452c1bfe90a54a2c7ce
Bill Tihen <btihen@gmail.com>
2019-06-24 15:13:11 +0200

    Admin is always hightlighted for all admin paths on Top Navbar

--- engines/core/app/helpers/samurai/application_helper.rb
+++ engines/core/app/helpers/samurai/application_helper.rb
@@ -24,14 +24,10 @@ module Samurai
       end
 
       # https://stackoverflow.com/questions/1203892/how-can-i-find-out-the-current-route-in-rails
-      def active_path?(match_controller)
-        current_uri = request.env['PATH_INFO']
-        path = Rails.application.routes.recognize_path(current_uri)
-        controller = path[:controller]
-        # action = path[:action]
-        pp path 
-        pp controller
-        controller.include?(match_controller) ? 'active' : ''
+      def active_path(match_controller)
+        current_uri = request.path_info  # "/admin/users"
+        current_controller = current_uri.split('/').second
+        current_controller.eql?(match_controller) ? 'active' : ''
       end
 
     end
